3a15abfe11b304ac78039a19d24969504681fb55
RobertoSiracusa <rjsc15.7@gmail.com>
2024-04-18 12:33:02 -0400

    arregle botones

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
 
-    document.addEventListener('DOMContentLoaded', function() {
-        // Obtén los elementos <li> con la clase 'scroll'
-        var items = document.querySelectorAll('.scroll');
+document.addEventListener('DOMContentLoaded', function() {
+    // Obtén los elementos <li> con la clase 'scroll'
+    var items = document.querySelectorAll('.scroll');
 
-        // Agrega un controlador de clic a cada elemento <li>
-        items.forEach(function(item) {
-            item.addEventListener('click', function() {
-                // Obtiene el ID de la sección relacionada con el elemento <li>
-                var sectionId = this.id.replace('section-nosotros', '');
+    // Agrega un controlador de clic a cada elemento <li>
+    items.forEach(function(item) {
+        item.addEventListener('click', function() {
+            // Obtiene el ID de la sección relacionada con el elemento <li>
+            var sectionId = this.id.replace('section-nosotros', '');
 
-                // Realiza el desplazamiento suave a la sección correspondiente
-                document.getElementById(section-nosotros).scrollIntoView({
-                    behavior: 'smooth'
-                });
+            // Realiza el desplazamiento suave a la sección correspondiente
+            document.getElementById(section-nosotros).scrollIntoView({
+                behavior: 'smooth'
             });
         });
     });
-    const scrollToSectionBtn = document.getElementById('scrollToSectionBtn');
-    const targetSection = document.getElementById('targetSection');
-    
-    scrollToSectionBtn.addEventListener('click', function() {
-      const targetY = targetSection.offsetTop;
-      window.scrollTo({
-        top: targetY,
-        behavior: 'smooth'
-      });
+});
+
+const buttons = document.querySelectorAll('button[href]');
+
+buttons.forEach(button => {
+button.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    const targetSection = document.querySelector(this.getAttribute('href'));
+    const targetPosition = targetSection.offsetTop;
+
+    window.scrollTo({
+    top: targetPosition,
+    behavior: 'smooth'
     });
+});
+});
